e715b5560daf308e50ca1bcb0179990ffbfc43ee
Soorya <sooryasdk@gmail.com>
2024-07-11 16:26:44 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+import pandas as pd
+
 if __name__=="__main__":
     obj =DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path : str=os.path.join('artifaxts',"train.csv")
-    test_data_path : str=os.path.join('artifaxts',"test.csv")
-    raw_data_path : str=os.path.join('artifaxts',"data.csv")
+    train_data_path : str=os.path.join('artifacts',"train.csv")
+    test_data_path : str=os.path.join('artifacts',"test.csv")
+    raw_data_path : str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
