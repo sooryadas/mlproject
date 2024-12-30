e1a4116122d667a3a8e9f252c3077d3ccd92f902
Soorya <sooryasdk@gmail.com>
2024-12-30 10:40:36 +0530

    deployment

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = application
 def index():
     return render_template('index.html') 
 
+
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
